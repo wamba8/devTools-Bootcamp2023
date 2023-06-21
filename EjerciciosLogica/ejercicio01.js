8ebb665e4649da875c7b18679c24c39def32a994
Tatiana Andrea Roldan Rojo <taty@Tatianas-MacBook-Air.local>
2023-06-21 14:23:09 -0500

    ejercicio 1

---
+++ EjerciciosLogica/ejercicio01.js
@@ -0,0 +1,24 @@
+function next_pal(numero) {
+    let inicial = numero + 1
+    let new_pal = 0
+
+    for (let i = inicial; new_pal === 0; i++) {
+        let A = i.toString()
+        let B = []
+        for (let j = A.length; j > 0 ; j--) {
+            let C = A[j-1]
+            B.push(C)
+        }
+        let num_alr = B.join("")
+        let C = num_alr
+        if (A === C) {
+            new_pal = C;
+        } else {
+            new_pal = 0
+        }
+    }
+
+    console.log(new_pal)
+}
+
+next_pal(305)
