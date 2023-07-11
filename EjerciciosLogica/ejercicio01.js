48d1737884f16458d3fc6ba235cf449050ef303c
Tatiana Andrea Roldan Rojo <taty@Tatianas-MacBook-Air.local>
2023-07-11 18:07:35 -0500

    correccion ejercicio 1: no recibe numeros negativos, cambiar console.log por return

--- EjerciciosLogica/ejercicio01.js
+++ EjerciciosLogica/ejercicio01.js
@@ -1,24 +1,29 @@
 function next_pal(numero) {
-    let inicial = numero + 1
-    let new_pal = 0
+    if (numero >= 0){
+        let inicial = numero + 1
+        let new_pal = 0
 
-    for (let i = inicial; new_pal === 0; i++) {
-        let A = i.toString()
-        let B = []
-        for (let j = A.length; j > 0 ; j--) {
-            let C = A[j-1]
-            B.push(C)
-        }
-        let num_alr = B.join("")
-        let C = num_alr
-        if (A === C) {
-            new_pal = C;
-        } else {
+        for (let i = inicial; new_pal === 0; i++) {
+            let A = i.toString()
+            let B = []
+            for (let j = A.length; j > 0 ; j--) {
+                let C = A[j-1]
+                B.push(C)
+            }
+            let num_alr = B.join("")
+            let C = num_alr
+            if (A === C) {
+                new_pal = C;
+            } else {
             new_pal = 0
+             }
         }
-    }
 
-    console.log(new_pal)
+        return new_pal
+    } else {
+        return "El numero ingresado es negativo"
+    }
+    
 }
 
-next_pal(305)
+console.log(next_pal(-200))
